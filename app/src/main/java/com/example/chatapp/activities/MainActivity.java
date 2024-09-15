c3cbbd0dd4e34172188979b6630e9be907c7fd71
RedwanParvez100 <19202103100@cse.bubt.edu.bd>
2024-09-15 20:02:04 +0600

    Working With FCM Token & Sign Out (half done)
    new icon added , image & name added

--- app/src/main/java/com/example/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.example.chatapp.utilities.Constants;
+import com.example.chatapp.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //  setContentView(R.layout.activity_main);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+    }
+
+    private void loadUserDetails(){
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
     }
 }
